2aceb538a44120f90ffbbee868b878b52e09571e
AhmadBShaik <ahmad.shaik1106@gmail.com>
2022-01-16 11:22:21 -0500

    add converters (json to csv, json to excel)

---
+++ src/converters/convert2csv.js
@@ -0,0 +1,16 @@
+const converter = require('json-2-csv');
+const fs = require('fs');
+
+
+const data = JSON.parse(fs.readFileSync('./src/output/result.json'));
+
+
+converter.json2csvAsync(data).then(csv => {
+
+    
+    console.log(csv);
+
+    
+    fs.writeFileSync('InstaData.csv', csv);
+
+}).catch(err => console.log(err));
