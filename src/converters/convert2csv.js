e5bf11849c6fa841545e86f41cf8cb7c96e38995
AhmadBShaik <ahmad.shaik1106@gmail.com>
2022-01-22 14:03:27 -0500

    stop console logging while exporting data into csv

--- src/converters/convert2csv.js
+++ src/converters/convert2csv.js
@@ -7,10 +7,6 @@ const data = JSON.parse(fs.readFileSync('./src/output/convertable.json'));
 
 converter.json2csvAsync(data).then(csv => {
 
-    
-    console.log(csv);
-
-    
     fs.writeFileSync('InstaData.csv', csv);
 
 }).catch(err => console.log(err));
